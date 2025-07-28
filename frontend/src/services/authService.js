5359dfe4fa5f7ca1ffef76f9242e73b812ade9d0
Andres Julian Arguelles Manco Arguelles <148500308+AndresArguelles23@users.noreply.github.com>
2025-07-27 21:23:16 -0500

    Remove frontend and backend directories

--- frontend/src/services/authService.js
+++
@@ -1,9 +0,0 @@
-// src/services/authService.js
-import axios from 'axios';
-
-const API_URL = 'http://localhost:4000/api/auth';
-
-export const loginUsuario = async (credenciales) => {
-  const res = await axios.post(`${API_URL}/login`, credenciales);
-  return res.data; // { token, usuario }
-};
